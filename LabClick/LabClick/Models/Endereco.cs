a9db01a6e3b47b9b9031ae4b7b2c85a0bbab49b7
Ronye do Lago <ronye.rocha@gmail.com>
2018-05-30 16:02:54 -0300

    models

---
+++ LabClick/LabClick/Models/Endereco.cs
@@ -0,0 +1,13 @@
+﻿namespace LabClick.Models
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string Cep { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Logradouro { get; set; }
+    }
+}
---
+++ LabClick/LabClick/Models/Paciente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LabClick.Models
+{
+    public class Paciente
+    {
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Cpf { get; set; }
+        public int EnderecoId { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public virtual Endereco Endereco { get; set; }
+    }
+}
