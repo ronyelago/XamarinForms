6c922ed8827a8ced89fdb686eaa7dcd498ebf04d
Ronye do Lago <ronye.rocha@gmail.com>
2018-05-30 15:57:33 -0300

    pacienteViewModel

---
+++ LabClick/LabClick/ViewModels/PacienteViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LabClick.ViewModels
+{
+    public class PacienteViewModel
+    {
+        public int ClinicaId { get; set; }
+        public int EnderecoId { get; set; }
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public string Cep { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Logradouro { get; set; }
+    }
+}
--- LabClick/LabClick/Views/NovoPacientePage.xaml.cs
+++ LabClick/LabClick/Views/NovoPacientePage.xaml.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace LabClick.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class NovoPacientePage : ContentPage
 	{
 		public NovoPacientePage ()
