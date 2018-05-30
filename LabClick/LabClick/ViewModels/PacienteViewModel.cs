81216cb50bc9d0fb2a69f8db57503a7feecaa359
Ronye do Lago <ronye.rocha@gmail.com>
2018-05-30 20:11:00 -0300

    other models

--- LabClick/LabClick/ViewModels/PacienteViewModel.cs
+++ LabClick/LabClick/ViewModels/PacienteViewModel.cs
@@ -1,23 +1,16 @@
-﻿using System;
+﻿using LabClick.Models;
 
 namespace LabClick.ViewModels
 {
     public class PacienteViewModel
     {
-        public int ClinicaId { get; set; }
-        public int EnderecoId { get; set; }
-        public string Nome { get; set; }
-        public string Sexo { get; set; }
-        public string Email { get; set; }
-        public string Telefone { get; set; }
-        public string Cpf { get; set; }
-        public DateTime DataNascimento { get; set; }
+        public PacienteViewModel()
+        {
+            Paciente = new Paciente();
+            Endereco = new Endereco();
+        }
 
-        public string Cep { get; set; }
-        public string Cidade { get; set; }
-        public string UF { get; set; }
-        public int Numero { get; set; }
-        public string Bairro { get; set; }
-        public string Logradouro { get; set; }
+        public Paciente Paciente { get; set; }
+        public Endereco Endereco { get; set; }
     }
 }
--- LabClick/LabClick/Views/NovoPacientePage.xaml.cs
+++ LabClick/LabClick/Views/NovoPacientePage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using LabClick.Models;
+using LabClick.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -19,7 +24,32 @@ namespace LabClick.Views
 
         private void btnSalvar_Clicked(object sender, EventArgs e)
         {
+            PacienteViewModel pacienteViewModel = new PacienteViewModel();
+            pacienteViewModel.Paciente.Nome = $"{txtNome.Text} {txtSobrenome.Text}";
+            pacienteViewModel.Paciente.DataNascimento = dataNascimento.Date;
+            pacienteViewModel.Paciente.Sexo = pickerGenero.SelectedItem.ToString();
+            pacienteViewModel.Paciente.Cpf = "234.234.234-23";
+            pacienteViewModel.Endereco.Cep = txtCep.Text;
+            pacienteViewModel.Endereco.UF = txtUf.Text;
+            pacienteViewModel.Endereco.Cidade = txtCidade.Text;
+            pacienteViewModel.Endereco.Bairro = txtBairro.Text;
+            pacienteViewModel.Endereco.Logradouro = txtRua.Text;
+            pacienteViewModel.Endereco.Numero = int.Parse(txtNumero.Text);
 
+            Paciente paciente = pacienteViewModel.Paciente;
+            var pacienteSerialized = JsonConvert.SerializeObject(paciente);
+
+            HttpClient client = new HttpClient();
+            Uri uri = new Uri(@"http://192.168.0.15:3000/paciente/pacientes");
+            var content = new StringContent(pacienteSerialized, Encoding.UTF8, "application/json");
+
+            var result = client.PostAsync(uri, content);
+
+            if (result.Result.IsSuccessStatusCode)
+            {
+                DisplayAlert("Sucesso", "Paciente cadastrado com sucesso!", "Ok");
+                Navigation.PushAsync(new MainPage());
+            }
         }
     }
 }
