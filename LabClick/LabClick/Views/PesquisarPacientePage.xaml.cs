9562a63807868ddcab72e9dd8f8916ab0d7066b2
Ronye do Lago <ronye.rocha@gmail.com>
2018-06-05 15:59:36 -0300

    busca pacientes works!

--- LabClick/LabClick/Views/PesquisarPacientePage.xaml.cs
+++ LabClick/LabClick/Views/PesquisarPacientePage.xaml.cs
@@ -1,4 +1,7 @@
 ﻿
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,5 +14,12 @@ namespace LabClick.Views
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private async Task PacienteSearchBar_SearchButtonPressed(object sender, System.EventArgs e)
+        {
+            var client = new HttpClient();
+            var result = await client.GetAsync($@"http://192.168.0.15:3000/paciente/getById/{PacienteSearchBar.Text}");
+            var content = result.Content.ReadAsStringAsync();
+        }
+    }
 }
