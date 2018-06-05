27d11b8143783ba25d85269efaebc6424067082e
Ronye do Lago <ronye.rocha@gmail.com>
2018-06-05 18:54:36 -0300

    busca works!

--- LabClick/LabClick/Views/PesquisarPacientePage.xaml.cs
+++ LabClick/LabClick/Views/PesquisarPacientePage.xaml.cs
@@ -1,5 +1,6 @@
-﻿
-using System;
+﻿using LabClick.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -18,8 +19,19 @@ namespace LabClick.Views
         private async Task PacienteSearchBar_SearchButtonPressed(object sender, System.EventArgs e)
         {
             var client = new HttpClient();
-            var result = await client.GetAsync($@"http://192.168.0.15:3000/paciente/getById/{PacienteSearchBar.Text}");
+            var result = await client.GetAsync($@"http://192.168.0.15:3000/paciente/getByName={PacienteSearchBar.Text}");
             var content = result.Content.ReadAsStringAsync();
+
+            var pacientes = JsonConvert.DeserializeObject<List<Paciente>>(content.Result);
+
+            List<string> nomes = new List<string>();
+
+            foreach (var p in pacientes)
+            {
+                nomes.Add(p.Nome);
+            }
+
+            PacientesListView.ItemsSource = nomes;
         }
     }
 }
