0c18062000e87c86307d01e6a109b525d8346c75
Ronye do Lago <ronye.rocha@gmail.com>
2018-06-12 17:57:01 -0300

    ILoadingPageService

---
+++ LabClick/LabClick/Serivices/Loading/ILoadingPageService.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace LabClick.Serivices.Loading
+{
+    public interface ILoadingPageService
+    {
+        void InitLoadingPage(ContentPage loadingIndicatorPage = null);
+
+        void ShowLoadingPage();
+
+        void HideLoadingPage();
+    }
+}
