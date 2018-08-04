b9043efbfae4aeeeeb2a02cdca7227ecf3e363f2
Ronye do Lago <ronye.rocha@gmail.com>
2018-08-03 21:03:13 -0300

    last

--- LabClick/LabClick/Views/Paciente/PesquisarPaciente.xaml.cs
+++ LabClick/LabClick/Views/Paciente/PesquisarPaciente.xaml.cs
@@ -12,11 +12,13 @@ namespace LabClick.Views.Paciente
 	public partial class PesquisarPaciente : ContentPage
 	{
         List<Domain.Entities.Paciente> pacientes = new List<Domain.Entities.Paciente>();
+        public List<SearchListViewItem> ListSearchItem { get; set; }
 
         public PesquisarPaciente ()
 		{
 			InitializeComponent ();
-		}
+
+        }
 
         private async Task PacienteSearchBar_SearchButtonPressed(object sender, System.EventArgs e)
         {
@@ -24,22 +26,39 @@ namespace LabClick.Views.Paciente
             var result = await client.GetAsync($@"http://apilabclick.mflogic.com.br/paciente/getByName={PacienteSearchBar.Text}");
             var content = result.Content.ReadAsStringAsync();
 
+            // cria lista com retorno da busca
             pacientes = JsonConvert.DeserializeObject<List<Domain.Entities.Paciente>>(content.Result);
 
-            //melhorar isso aqui!
-            List<string> nomes = new List<string>();
+            // lista de itens que possuem nome e cor
+            ListSearchItem = new List<SearchListViewItem>();
 
-            foreach (var p in pacientes)
+            // inserindo os nomes e números 
+            for (int i = 0; i < pacientes.Count; i++)
             {
-                nomes.Add(p.Nome);
+                ListSearchItem.Add(new SearchListViewItem { Name = pacientes[i].Nome, SetColor = i });
             }
-            
-            PacientesListView.ItemsSource = nomes;
+
+            foreach (var item in ListSearchItem)
+            {
+                if (item.SetColor % 2 == 0)
+                {
+                    item.Color = Color.FromHex("#EAF4F5");
+                }
+                else
+                {
+                    item.Color = Color.White;
+                }
+            }
+
+            PacientesListView.ItemsSource = ListSearchItem;
         }
 
         private void PacientesListView_ItemTapped(object sender, ItemTappedEventArgs e)
         {
-            var paciente = pacientes.FirstOrDefault(p => p.Nome == PacientesListView.SelectedItem.ToString());
+            var item = (SearchListViewItem)e.Item;
+            item.Color = Color.Blue;
+
+            var paciente = pacientes.FirstOrDefault(p => p.Nome == item.Name);
             var pacienteMasterPage = new PacienteMasterPage(paciente);
             App.MasterPage = pacienteMasterPage;
 
---
+++ LabClick/LabClick/Views/Paciente/SearchListViewItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace LabClick.Views.Paciente
+{
+    public class SearchListViewItem
+    {
+        public string Name { get; set; }
+        public Color Color { get; set; }
+        public int SetColor { get; set; }
+    }
+}
