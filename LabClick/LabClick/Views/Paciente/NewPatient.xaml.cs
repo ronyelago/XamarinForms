1814c0eca9dcf1f1d4f1a67e7600a75ff4e7e021
Ronye do Lago <ronye.rocha@gmail.com>
2018-08-17 12:33:18 -0300

    new patient

---
+++ LabClick/LabClick/Views/Paciente/NewPatient.xaml.cs
@@ -0,0 +1,21 @@
+﻿using LabClick.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LabClick.Views.Paciente
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewPatient : ContentPage
+	{
+		public NewPatient ()
+		{
+			InitializeComponent ();
+            this.BindingContext = new NewPatientViewModel();
+		}
+
+        private void BtnSalvar_Clicked(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
