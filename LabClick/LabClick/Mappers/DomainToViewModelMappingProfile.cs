c4b676892fe3abc37eb12323f0c4e39fcd5386b4
Ronye do Lago <ronye.rocha@gmail.com>
2018-08-21 11:35:04 -0300

    mapping ok

--- LabClick/LabClick/Mappers/DomainToViewModelMappingProfile.cs
+++ LabClick/LabClick/Mappers/DomainToViewModelMappingProfile.cs
@@ -9,8 +9,12 @@ namespace LabClick.Mappers
         {
             CreateMap<Domain.Entities.Paciente, PacienteViewModel>();
             CreateMap<Domain.Entities.Paciente, NewPatientViewModel>()
-                .ForMember(end => end.Cep,
-                           opt => opt.MapFrom( e => e.Endereco.Cep));
+                .ForMember(end => end.Cep, opt => opt.MapFrom(p => p.Endereco.Cep))
+                .ForMember(end => end.UF, opt => opt.MapFrom(p => p.Endereco.UF))
+                .ForMember(end => end.Cidade, opt => opt.MapFrom(p => p.Endereco.Cidade))
+                .ForMember(end => end.Bairro, opt => opt.MapFrom(p => p.Endereco.Bairro))
+                .ForMember(end => end.Logradouro, opt => opt.MapFrom(p => p.Endereco.Logradouro))
+                .ForMember(end => end.Numero, opt => opt.MapFrom(p => p.Endereco.Numero));
             CreateMap<Domain.Entities.Endereco, NewPatientViewModel>();
             CreateMap<Domain.Entities.Teste, DigitalizarTesteViewModel>();
         }
