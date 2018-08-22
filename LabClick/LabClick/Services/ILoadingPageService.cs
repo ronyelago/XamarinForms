8759149db9f8d6fa32ef96478091e3d79097dd22
Ronye do Lago <ronye.rocha@gmail.com>
2018-08-22 15:53:01 -0300

    add interface

---
+++ LabClick/LabClick/Services/ILoadingPageService.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace LabClick.Services
+{
+    interface ILoadingPageService
+    {
+        void InitLoadingPage
+              (ContentPage loadingIndicatorPage = null);
+
+        void ShowLoadingPage();
+
+        void HideLoadingPage();
+    }
+}
