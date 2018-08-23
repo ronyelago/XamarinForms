9e14262a4d036ccfb94324c8f5e2c4d6d4102f25
Ronye do Lago <ronye.rocha@gmail.com>
2018-08-23 13:00:26 -0300

    TestListViewModel

---
+++ LabClick/LabClick/Views/Teste/TestListViewItem.cs
@@ -0,0 +1,11 @@
+﻿namespace LabClick.Views.Teste
+{
+    public class TestListViewItem
+    {
+        public string NomeTeste { get; set; }
+        public string NomePaciente { get; set; }
+        public int PacienteId { get; set; }
+        public string Status { get; set; }
+        public string HexColor { get; set; }
+    }
+}
