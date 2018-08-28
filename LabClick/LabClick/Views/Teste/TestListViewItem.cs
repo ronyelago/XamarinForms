274718827dc12539e2cff638c3abc0a6f8f852b1
Ronye do Lago <ronye.rocha@gmail.com>
2018-08-28 18:41:39 -0300

    single

--- LabClick/LabClick/Views/Teste/TestListViewItem.cs
+++ LabClick/LabClick/Views/Teste/TestListViewItem.cs
@@ -8,6 +8,9 @@ namespace LabClick.Views.Teste
         public string NomePaciente { get; set; }
         public int PacienteId { get; set; }
         public string Status { get; set; }
+        public string Resultado { get; set; }
+        public string ResultadoDetalhes { get; set; }
+        public byte[] LaudoPdf { get; set; }
         public Color Color { get; set; }
         public int SetColor { get; set; }
         public string DataTeste { get; set; }
