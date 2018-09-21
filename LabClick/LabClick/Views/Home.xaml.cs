432bdc31ff7cdb6a10d8eb014ed41cf1b19aec8c
Ronye do Lago <ronye.rocha@gmail.com>
2018-09-21 16:32:35 -0300

    listaLaudosImg

--- LabClick/LabClick/Views/Home.xaml.cs
+++ LabClick/LabClick/Views/Home.xaml.cs
@@ -14,37 +14,32 @@ namespace LabClick.Views
 
             if (Device.Idiom == TargetIdiom.Phone)
             {
-                this.MainStack.WidthRequest = 200;
-                this.MainStack.HeightRequest = 400;
+                
             }
         }
 
         // Cada um dos eventos abaixo passa um número como parâmetro
         // quando estancia a MainTab que indica qual página deve ser
         // selecionada para exibição
-        //private void NovoTeste_Clicked(object sender, System.EventArgs e)
-        //{
-        //    Navigation.PushAsync(new MainTab(0));
-        //}
-
-        private void PesquisarPaciente_Clicked(object sender, System.EventArgs e)
-        {
-            Navigation.PushAsync(new MainTab(1));
-        }
-
-        private void NovoPaciente_Clicked(object sender, System.EventArgs e)
-        {
-            Navigation.PushAsync(new MainTab(2));
-        }
-
-        private void PesquisarLaudo_Clicked(object sender, System.EventArgs e)
-        {
-            Navigation.PushAsync(new MainTab(3));
-        }
 
         private void DigitalizarExameImg_Tapped(object sender, System.EventArgs e)
         {
             Navigation.PushAsync(new MainTab(0));
+        }
+
+        private void PesquisarPacienteImg_Tapped(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MainTab(1));
+        }
+
+        private void NovoPacienteImg_Tapped(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MainTab(2));
+        }
+
+        private void listaLaudosImg_Tapped(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MainTab(3));
         }
     }
 }
