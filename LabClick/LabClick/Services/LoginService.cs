91965a209e51d1d72ecb1b31d0cc1c0c141173f9
Ronye do Lago <ronye.rocha@gmail.com>
2018-10-09 14:03:14 -0300

    LoginViewModel

--- LabClick/LabClick/Services/LoginService.cs
+++ LabClick/LabClick/Services/LoginService.cs
@@ -1,7 +1,12 @@
-﻿namespace LabClick.Services
+﻿using LabClick.Domain.Entities;
+
+namespace LabClick.Services
 {
     public class LoginService
     {
+        public bool Login(Usuario usuario)
+        {
 
+        }
     }
 }
