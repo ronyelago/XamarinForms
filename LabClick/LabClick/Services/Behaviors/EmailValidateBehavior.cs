96792ca670f5f204de4b18f088fe9febb5bcec83
Ronye do Lago <ronye.rocha@gmail.com>
2018-10-19 20:32:13 -0300

    las

--- LabClick/LabClick/Services/Behaviors/EmailValidateBehavior.cs
+++ LabClick/LabClick/Services/Behaviors/EmailValidateBehavior.cs
@@ -1,9 +1,38 @@
-﻿using Xamarin.Forms;
+﻿using System.Text.RegularExpressions;
+using Xamarin.Forms;
 
 namespace LabClick.Services.Behaviors
 {
     public class EmailValidateBehavior : Behavior<Entry>
     {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
 
+            bindable.TextChanged += BindableOnTextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            bindable.TextChanged -= BindableOnTextChanged;
+        }
+
+        private void BindableOnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string email = e.NewTextValue;
+            string emailPattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+            Entry emailEntry = sender as Entry;
+
+            if (Regex.IsMatch(email, emailPattern))
+            {
+                emailEntry.BackgroundColor = Color.Transparent;
+            }
+            else
+            {
+                emailEntry.BackgroundColor = Color.Red;
+            }
+        }
     }
 }
